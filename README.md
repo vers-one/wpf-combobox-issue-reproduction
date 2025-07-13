9c4d499a5cf48626d271ac24497c6a307e8bff0e
vers-one <12114169+vers-one@users.noreply.github.com>
2025-07-13 04:18:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Reproduction project for [WPF ComboBox issue](https://github.com/dotnet/wpf/issues/10966)
+
+## Reproduction
+
+1. Open the `ComboBoxIssueReproduction.sln` project in Visual Studio 2022.
+2. Build and run the project.
+3. Follow reproduction steps listed in the main window.
---
+++ Source/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace ComboBoxIssueReproduction
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ObservableCollection<string> items = [];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            comboBox.Width = 250;
+            items.Add("Item 1");
+            comboBox.ItemsSource = items;
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (resizeCheckBox.IsChecked == true)
+            {
+                comboBox.Width -= 10;
+            }
+            items.Add("Item 2");
+        }
+    }
+}
